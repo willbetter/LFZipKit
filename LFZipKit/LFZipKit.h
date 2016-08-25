0ec775e5fbe538a9dccc63d1527f2109d70c1cd3
wangxiaoxiang <wangxiaoxiang-cn@foxmail.com>
2016-08-25 12:20:41 +0800

    init

---
+++ LFZipKit/LFZipKit.h
@@ -0,0 +1,21 @@
+//
+//  LFZipKit.h
+//  LFZipKit
+//
+//  Created by wangxiaoxiang on 16/8/25.
+//  Copyright © 2016年 wangxiaoxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LFZipKit.
+FOUNDATION_EXPORT double LFZipKitVersionNumber;
+
+//! Project version string for LFZipKit.
+FOUNDATION_EXPORT const unsigned char LFZipKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LFZipKit/PublicHeader.h>
+
+
+#import <LFZipKit/LFZipArchive.h>
+#import <LFZipKit/unzip.h>
